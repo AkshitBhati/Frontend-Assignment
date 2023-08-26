1ecb91450d451d95c6cdfb702bf71baaa0792eba
AkshitBhati <akshitworks24@gmail.com>
2023-08-27 00:28:50 +0530

    styling forms

--- AkshitBhati/src/Components/SignIn/SignIn.jsx
+++ AkshitBhati/src/Components/SignIn/SignIn.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./SignIn.css"
+import { PiFacebookLogo } from "react-icons/pi"
+import { PiGoogleLogoThin } from "react-icons/pi"
+import { PiLinkedinLogo } from "react-icons/pi"
 
 
 const SignIn = () => {
@@ -15,11 +18,24 @@ const SignIn = () => {
             <input type="password" />
             <button className='form__btn'>Continue</button>
             <p className='form__or'>or</p>
-            <button className='form__btn--social'>Continue with facebook</button>
-            <button className='form__btn--social'>Continue with Google</button>
-            <button className='form__btn--social'>Continue with Linkedin</button>
+            
+            <div className='form__btn--social'>
+            <PiFacebookLogo className='formBtn__logo'/>
+            <p>Continue with Facebook</p>
+            </div>
+
+            <div className='form__btn--social'>
+            <PiGoogleLogoThin className='formBtn__logo'/>
+            <p>Continue with Google</p>
+            </div>
+            <div className='form__btn--social'>
+            <PiLinkedinLogo className='formBtn__logo'/>
+            <p>Continue with Linkedin</p>
+            </div>
+            
+            
         </form>
-        <p>By Signing up, you agree our <span>Terms and Condition</span></p>
+        <p className='signIn__conditions'>By Signing up, you agree our <span>Terms and Condition</span></p>
     </div>
   )
 }
